dee90f216a3e45704633e22b3e39a523a561b23b
Gürkan Gökdemir <gurkangokdemir@outlook.com>
2019-03-30 21:09:33 +0300

    Update main.sql

--- db/main.sql
+++ db/main.sql
@@ -1,1 +1,28 @@
 
+CREATE DATABASE NewsDB;
+
+CREATE TABLE News{
+newsId int not null,
+newsTitle varchar,
+newsContent varchar,
+newsTopic varchar,
+newsImage image,
+newsDate date
+PRIMARY KEY(newsId)
+}
+
+CREATE TABLE NewsRate{
+newsId int not null,
+likes int,
+dislikes int,
+viewCount int
+PRIMARY KEY(newsId)
+FOREIGN KEY(newsId) REFERENCES News(newsId)
+}
+
+CREATE TABLE UserInfo{
+userId int not null,
+userName varchar,
+userPass password
+PRIMARY KEY()
+}
