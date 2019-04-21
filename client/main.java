57a814fcb69504ad0135c262da1417b5481f9c8d
Gürkan Gökdemir <gurkangokdemir@outlook.com>
2019-04-21 12:22:27 +0300

    Update main.java

--- client/main.java
+++ client/main.java
@@ -1,1 +1,32 @@
+int newsId;
+Image newsImage;
+String newsTitle;
+String newsContent;
+String newsTopic;
+Date newsDate;
 
+int likes;
+int dislikes;
+int count;
+
+int userId;
+String userName;
+int userPass;
+
+public class MainLayout{
+    public void ShowPopup(){}
+    public void ShowContentWhenClickedToPopup(){}
+}
+
+public class LoginLayout{
+    public void GoToMainLayout(){}
+}
+
+public class NewsLayout{
+    public void LikeContent(){}
+    public void DislikeContent(){}
+    public void ShowLikes(){}
+    public void ShowDislikes(){}
+    public void ShowCount(){}
+    public void ShowContent(){}
+}
